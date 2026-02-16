c4007d9ff293be1b2fd817c555808bb8fc043701
chionye <michael.chionye@gmail.com>
2026-02-16 12:23:33 +0100

    update api endpoint

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -5,7 +5,7 @@ import { useAuthStore } from "../stores/authStore";
 
 const BASE_URL =
   import.meta.env.VITE_API_BASE_URL ||
-  "http://localhost:3000/api/v1";
+  "https://questbackendv2.onrender.com/api/v1";
 
 const api = axios.create({
   baseURL: BASE_URL,
