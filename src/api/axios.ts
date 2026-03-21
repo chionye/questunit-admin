7d22d9f0955321ae5e397bd1d791ac30c9fe63a6
chionye <michael.chionye@gmail.com>
2026-03-21 20:49:36 +0100

    fix 404 error on prod

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -31,7 +31,7 @@ api.interceptors.response.use(
   (error) => {
     if (error.response?.status === 401) {
       useAuthStore.getState().logout();
-      window.location.href = '/login';
+      window.location.href = '/';
     }
     return Promise.reject(error);
   }
